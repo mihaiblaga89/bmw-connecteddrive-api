757a825d3b38425c114372c915e1224b67f7a911
Mihai Blaga <legolas8911@gmail.com>
2019-09-23 00:29:30 +0200

    initial

---
+++ example/index.js
@@ -0,0 +1,7 @@
+import API from '../src';
+
+API.init({
+  region: 'eu',
+  username: 'a',
+  password: 'b',
+});
---
+++ src/api/index.js
@@ -0,0 +1,83 @@
+/* eslint-disable camelcase */
+import axios from 'axios';
+import querystring from 'querystring';
+import moment from 'moment';
+
+import BMWURLs from '../helpers/urls';
+
+class API {
+  async init({ region, username, password }) {
+    this.region = region;
+    this.username = username;
+    this.password = password;
+    this.vehicles = {};
+    this.BMWURLs = new BMWURLs(region);
+    this.oauthToken = null;
+    this.refreshToken = null;
+    this.tokenExpiresAt = null;
+
+    await this.getVehicles();
+  }
+
+  async request(url) {
+    if (
+      !this.oauthToken ||
+      (this.tokenExpiresAt && moment().isAfter(this.tokenExpiresAt))
+    ) {
+      await this.getToken();
+    }
+
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36',
+    };
+
+    headers.accept = 'application/json';
+    headers.Authorization = `Bearer ${this.oauthToken}`;
+    headers.referer = 'https://www.bmw-connecteddrive.de/app/index.html';
+
+    return axios.get(url, {
+      headers,
+    });
+  }
+
+  async getToken() {
+    const { username, password } = this;
+    const postData = querystring.stringify({
+      grant_type: 'password',
+      scope: 'authenticate_user vehicle_data remote_services',
+      username,
+      password,
+    });
+
+    const headers = {
+      'Content-Length': '124',
+      Connection: 'Keep-Alive',
+      Host: this.BMWURLs.getHost(),
+      'Accept-Encoding': 'gzip',
+      Authorization:
+        'Basic blF2NkNxdHhKdVhXUDc0eGYzQ0p3VUVQOjF6REh4NnVuNGNEanliTEVOTjNreWZ1bVgya0VZaWdXUGNRcGR2RFJwSUJrN3JPSg==',
+      Credentials:
+        'nQv6CqtxJuXWP74xf3CJwUEP:1zDHx6un4cDjybLENN3kyfumX2kEYigWPcQpdvDRpIBk7rOJ',
+      'User-Agent': 'okhttp/2.60',
+    };
+
+    const { data } = await axios.post(this.BMWURLs.getAuthURL(), postData, {
+      headers,
+    });
+
+    const { access_token, expires_in } = data;
+
+    this.oauthToken = access_token;
+    this.tokenExpiresAt = moment().add(expires_in, 'seconds');
+  }
+
+  async getVehicles() {
+    const { data } = await this.request(this.BMWURLs.getVehiclesURL());
+
+    console.log('VEHICLES', data);
+  }
+}
+
+export default new API();
